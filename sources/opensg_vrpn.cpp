5bd69efb4c5a86403364b6973b65ea6cb8717f2d
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-09-14 17:29:08 +0200

    copy correct file content of old project

--- sources/opensg_vrpn.cpp
+++ sources/opensg_vrpn.cpp
@@ -1,299 +1,197 @@
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
-//#define SNIPPET_STEP 7
+#include <cstdlib>
+#include <cstddef>
+#include <cmath>
+#include <iostream>
+#include <ios>
 
-#ifdef _MSC_VER
-# pragma warning(push)
-# pragma warning(disable:4251 4275 4290 4996 4231 4244)
-#endif
-// OpenSG includes
 #include <OpenSG/OSGGLUT.h>
 #include <OpenSG/OSGConfig.h>
-#include <OpenSG/OSGGLUTWindow.h>
-#include <OpenSG/OSGSimpleSceneManager.h>
 #include <OpenSG/OSGSimpleGeometry.h>
-#include <OpenSG/OSGComponentTransform.h>
-#include <OpenSG/OSGMaterialGroup.h>
-#include <OpenSG/OSGImage.h>
-#include <OpenSG/OSGSimpleTexturedMaterial.h>
+#include <OpenSG/OSGGLUTWindow.h>
+#include <OpenSG/OSGMultiDisplayWindow.h>
 #include <OpenSG/OSGSceneFileHandler.h>
-#include <OpenSG/OSGTextureBackground.h>
-#include <OpenSG/OSGGradientBackground.h>
-#include <OpenSG/OSGPointLight.h>
-#include <OpenSG/OSGSpotLight.h>
-#include <ctime>
+#include <OpenSG/OSGSwitch.h>
 
-#ifdef _MSC_VER
-# pragma warning(pop)
-#endif
+#include <OSGCSM/OSGCAVESceneManager.h>
+#include <OSGCSM/OSGCAVEConfig.h>
+#include <OSGCSM/appctrl.h>
 
-OSG_USING_NAMESPACE // activate the OpenSG namespace
+#include "Arguments.h"
+#include "Scene.h"
+#include "input/RemoteManager.h"
 
-//------------------------------------------------------------------------------
-// Global Variables
-//------------------------------------------------------------------------------
-SimpleSceneManagerRefPtr mgr; // the SimpleSceneManager to manage applications
-NodeRecPtr beachTrans;
+OSG_USING_NAMESPACE
 
-//------------------------------------------------------------------------------
-// Forward Declarations
-//------------------------------------------------------------------------------
-// forward declaration so we can have the interesting parts upfront
-int setupGLUT(int *argc, char *argv[]);
+OSGCSM::CAVEConfig cfg;
+OSGCSM::CAVESceneManager *mgr = nullptr;
 
-// forward declaration to cleanup the used modules and databases
-void cleanup();
+input::RemoteManager* remoteManager;
 
-NodeTransitPtr createScenegraph() {
-    NodeRecPtr root = Node::create();
-    root->setCore(Group::create());
+Scene* scene = nullptr;
 
-    NodeRecPtr boxChild = makeBox(5,4,4,1,1,1);
-    NodeRecPtr beach = makePlane(30, 30, 1, 1);
+// head light fix (1/3)
+DirectionalLightRecPtr mainLight;
 
-    GeometryRecPtr sunGeo = makeSphereGeo(2, 3);
-    NodeRecPtr sunChild = Node::create();
-    sunChild->setCore(sunGeo);
-
-    root->addChild(sunChild);
-    root->addChild(boxChild);
-    root->addChild(beach);
-
-    //decouple the nodes to be shifted in hierarchy from the scene
-    root->subChild(sunChild);
-    root->subChild(beach);
-
-    TransformRecPtr sunTransCore = Transform::create();
-    Matrix sunMatrix;
-
-    // Setting up the matrix
-    sunMatrix.setIdentity();
-    sunMatrix.setTranslate(0,20,0);
-    sunTransCore->setMatrix(sunMatrix); // Adding the Matrix to the core
-
-    // Setting up the node
-    NodeRecPtr sunTrans = makeNodeFor(sunTransCore);
-    sunTrans->addChild(sunChild);
-
-    ComponentTransformRecPtr ct = ComponentTransform::create();
-    ct->setTranslation(Vec3f(0,-2,0));
-    ct->setRotation(Quaternion(Vec3f(1,0,0),osgDegree2Rad(90)));
-
-    beachTrans = Node::create();
-    beachTrans->setCore(ct);
-    beachTrans->addChild(beach);
-
-    // put the nodes in the scene again
-    root->addChild(beachTrans);
-    root->addChild(sunTrans);
-    root->subChild(sunTrans);
-
-    SimpleMaterialRecPtr sunMat = SimpleMaterial::create();
-    sunMat->setDiffuse(Color3f(1,0.8f,0));
-    sunMat->setAmbient(Color3f(0.8f, 0.2f, 0.2f));
-
-    MaterialGroupRecPtr sunMgCore = MaterialGroup::create();
-    sunMgCore->setMaterial(sunMat);
-
-    NodeRecPtr sunMg = Node::create();
-
-    sunMg->setCore(sunMgCore);
-    sunMg->addChild(sunTrans);
-
-    root->addChild(sunMg);
-
-    SimpleMaterialRecPtr boxMat = SimpleMaterial::create();
-
-    boxMat->setDiffuse(Color3f(1,0.2f,0.1f));
-    boxMat->setAmbient(Color3f(0.8f, 0.2f, 0.2f));
-    boxMat->setTransparency(0.25);
-    //boxMat->setLit(false);
-
-    GeometryRecPtr boxGeo = dynamic_cast<Geometry*>(boxChild->getCore());
-    boxGeo->setMaterial(boxMat);
-    ImageRecPtr image = Image::create();
-    // sand taken from http://www.filterforge.com/filters/720.jpg
-    image->read("models/sand.jpg");
-
-    //now we create the texture that will hold the image
-    SimpleTexturedMaterialRecPtr tex = SimpleTexturedMaterial::create();
-    tex->setImage(image);
-
-    //now assign the fresh texture to the geometry
-    GeometryRecPtr beachGeo = dynamic_cast<Geometry*>(beach->getCore());
-    beachGeo->setMaterial(tex);
-
-    //model taken from http://storage3d.com/
-    NodeRecPtr palmTree = SceneFileHandler::the()->read("models/palm.3ds");
-
-    ComponentTransformRecPtr palmCT = ComponentTransform::create();
-    palmCT->setTranslation(Vec3f(12,-1,0));
-    palmCT->setRotation(Quaternion(Vec3f(0,1,0),osgDegree2Rad(45)));
-    palmCT->setScale(Vec3f(10.f,10.f,10.f));
-
-    NodeRecPtr palmTrans = makeNodeFor(palmCT);
-    palmTrans->addChild(palmTree);
-
-    root->addChild(palmTrans);
-
-    NodeRecPtr palmTree2 = OSG::deepCloneTree(palmTrans);
-    ComponentTransformRecPtr palmCT2 = dynamic_cast<ComponentTransform*>(palmTree2->getCore());
-
-    palmCT2->setTranslation(Vec3f(10,-1,5));
-    palmCT->setRotation(Quaternion(Vec3f(1,0,0),osgDegree2Rad(0)));
-    palmCT->setScale(Vec3f(10.f,10.f,10.f));
-
-    root->addChild(palmTree2);
-
-    PointLightRecPtr sunLight = PointLight::create();
-    //sunLight->setAttenuation(1,0,2);
-
-    //color information
-    sunLight->setDiffuse(Color4f(1,1,1,1));
-    sunLight->setAmbient(Color4f(0.2f,0.2f,0.2f,1));
-    sunLight->setSpecular(Color4f(1,1,1,1));
-
-    sunLight->setBeacon(sunChild); //attach to the sun node use this node as position beacon
-
-    root->setCore(sunLight);
-
-    DirectionalLightRecPtr dirLight = DirectionalLight::create();
-    dirLight->setDirection(1,1,-1);
-
-    //color information
-    dirLight->setDiffuse(Color4f(1,1,1,1));
-    dirLight->setAmbient(Color4f(0.2f,0.2f,0.2f,1));
-    dirLight->setSpecular(Color4f(1,1,1,1));
-
-
-    //wrap the root, cause only nodes below the lights will be lit
-    NodeRecPtr ueberroot = makeNodeFor(dirLight);
-    ueberroot->addChild(root);
-
-    root = ueberroot;
-    return NodeTransitPtr(root);
+void cleanup()
+{
+    delete mgr;
+    delete remoteManager;
 }
 
-Action::ResultE enter(Node* node){
-    if(node->getCore()->getType().isDerivedFrom(ComponentTransform::getClassType()))
-        std::cout << "Enter node : " << node << std::endl;
-    return Action::Continue;
+void keyboard(unsigned char k, int x, int y)
+{
+    Real32 ed;
+    switch(k)
+    {
+        case 'q':
+        case 27:
+            cleanup();
+            exit(EXIT_SUCCESS);
+            break;
+        case 'e':
+            ed = mgr->getEyeSeparation() * .9f;
+            std::cout << "Eye distance: " << ed << '\n';
+            mgr->setEyeSeparation(ed);
+            break;
+        case 'E':
+            ed = mgr->getEyeSeparation() * 1.1f;
+            std::cout << "Eye distance: " << ed << '\n';
+            mgr->setEyeSeparation(ed);
+            break;
+        case 'h':
+            cfg.setFollowHead(!cfg.getFollowHead());
+            std::cout << "following head: " << std::boolalpha << cfg.getFollowHead() << '\n';
+            break;
+        case 'i':
+            remoteManager->print_tracker();
+            break;
+        default:
+            std::cout << "Key '" << k << "' ignored\n";
+    }
 }
 
-Action::ResultE leave(Node* node, Action::ResultE result){
-//  std::cout << "Leaving node: " << node << "with code: " << result << std::endl;
-    return result;
+void setupGLUT(int *argc, char *argv[])
+{
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_RGB  |GLUT_DEPTH | GLUT_DOUBLE);
+    glutCreateWindow("OpenSG CSMDemo with VRPN API");
+    glutDisplayFunc([]()
+                    {
+                        // black navigation window
+                        glClear(GL_COLOR_BUFFER_BIT);
+                        glutSwapBuffers();
+                    });
+    glutReshapeFunc([](int w, int h)
+                    {
+                        mgr->resize(w, h);
+                        glutPostRedisplay();
+                    });
+    glutKeyboardFunc(keyboard);
+    glutIdleFunc([]()
+                 {
+                     remoteManager->check_tracker();
+
+                     scene->update();
+
+                     const auto speed = 1.f;
+                     auto head = remoteManager->head;
+                     mgr->setUserTransform(head.position, head.orientation);
+                     mgr->setTranslation(mgr->getTranslation() + speed * remoteManager->analog_values);
+
+                     // head light fix (2/3)
+                     Matrix4f mat;
+                     mat.setRotate(head.orientation);
+                     auto dir = mat * Vec3f(0.f, 0.f, 1.f);
+                     mainLight->setDirection(dir);
+
+                     commitChanges();
+                     mgr->redraw();
+                     // the changelist should be cleared - else things could be copied multiple times
+                     OSG::Thread::getCurrentChangeList()->clear();
+                 });
 }
-int main(int argc, char **argv) {
+
+int main(int argc, char **argv)
+{
 #if WIN32
     OSG::preloadSharedObject("OSGFileIO");
 	OSG::preloadSharedObject("OSGImageFileIO");
 #endif
+    try
     {
-        osgInit(argc, argv); // initialize OpenSG
+        // ChangeList needs to be set for OpenSG 1.4
+        ChangeList::setReadWriteDefault();
+        osgInit(argc,argv);
 
-        int winid = setupGLUT(&argc, argv);  // initialize GLUT
+        // evaluate intial params
+        Arguments args(argc, argv);
 
-        // the connection between GLUT and OpenSG is established
-        GLUTWindowRecPtr gwin = GLUTWindow::create();
-        gwin->setGlutId(winid);
-        gwin->init();
+        if (args.configurationFile == nullptr) {
+            throw "No configuration passed.";
+        } else if (!cfg.loadFile(args.configurationFile)) {
+            std::string file = args.configurationFile;
+            throw "ERROR: could not load config file '" + file + "'";
+        }
 
-        NodeRecPtr root = createScenegraph();
+        cfg.printConfig();
 
-        traverse(root,enter,leave);
+        // start servers for video rendering
+        if ( startServers(cfg) < 0 )
+        {
+            std::cout << "ERROR: Failed to start servers\n";
+            return EXIT_FAILURE;
+        }
 
-        mgr = OSG::SimpleSceneManager::create();
-        mgr->setWindow(gwin);			// tell the manager what to manage
-        mgr->setRoot(root);				// attach the scenegraph
+        setupGLUT(&argc, argv);
+
+        remoteManager = new input::RemoteManager(cfg);
+
+        MultiDisplayWindowRefPtr mwin = createAppWindow(cfg, cfg.getBroadcastaddress());
+
+        NodeRefPtr root = nullptr;
+        if (args.sceneFile != nullptr) {
+            root = SceneFileHandler::the()->read(args.sceneFile, NULL);
+            if (!root) {
+                throw "Scene could not be loaded: '" + std::string(args.sceneFile) + "'";
+            }
+        } else {
+            scene = new Scene(remoteManager->wand);
+            root = scene->root;
+        }
+
+        // head light fix (3/3)
+        mainLight = DirectionalLight::create();
+        root->setCore(mainLight);
+        NodeRecPtr beaconGeo = makeNodeFor(Geometry::create());
+        mainLight->setBeacon(beaconGeo);
+
+        commitChanges();
+
+        mgr = new OSGCSM::CAVESceneManager(&cfg);
+        mgr->setWindow(mwin );
+        mgr->setRoot(root);
+        mgr->showAll();
+        mgr->getWindow()->init();
+        mgr->turnWandOff();
         mgr->setHeadlight(false);
-        mgr->showAll();					// show the whole scene
-
-        ImageRecPtr backimage = Image::create();
-        backimage->read("models/tropical-beach.jpg");
-
-        TextureObjChunkRecPtr bkgTex = TextureObjChunk::create();
-        bkgTex->setImage(backimage);
-        bkgTex->setScale(false);
-        TextureBackgroundRecPtr imBkg = TextureBackground::create();
-        imBkg->setTexture(bkgTex);
-        imBkg->setColor(Color4f(1.0,1.0,1.0,0.0f));
-
-        // alternatively use a gradient background
-        //GradientBackgroundRecPtr bkg = GradientBackground::create();
-        //bkg->addLine(Color3f(0.7f, 0.7f, 0.8f), 0);
-        //bkg->addLine(Color3f(0.0f, 0.1f, 0.3f), 1);
-
-        gwin->getPort(0)->setBackground(imBkg);
     }
+    catch(std::string e)
+    {
+        std::cout << "ERROR: " << e << '\n';
+        return EXIT_FAILURE;
+    }
+    catch(const char *e)
+    {
+        std::cout << "ERROR: " << e << '\n';
+        return EXIT_FAILURE;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << "ERROR: " << e.what() << '\n';
+        return EXIT_FAILURE;
+    }
+
     glutMainLoop();
-}
-
-void display() {
-    const float time = 1000.f * std::clock() / CLOCKS_PER_SEC;
-
-    ComponentTransformRecPtr bt = dynamic_cast<ComponentTransform*>(beachTrans->getCore());
-
-    //bt->setTranslation(Vec3f(10,5,0));
-    bt->setRotation(Quaternion(Vec3f(1,0,0),osgDegree2Rad(270)+0.001f*time));
-    //bt->setScale(Vec3f(0.001,0.001,0.001));
-
-    //updateMesh(time);
-    commitChanges(); //make sure the changes are distributed over the containers
-    mgr->redraw(); // redraw the window
-}
-
-void reshape(int w, int h) {
-    mgr->resize(w, h); // react to size changes
-    glutPostRedisplay();
-}
-
-void mouse(int button, int state, int x, int y) {
-    // react to mouse button presses
-    if (state) {
-        mgr->mouseButtonRelease(button, x, y);
-    } else {
-        mgr->mouseButtonPress(button, x, y);
-    }
-    glutPostRedisplay();
-}
-
-void motion(int x, int y) {
-    mgr->mouseMove(x, y);
-    glutPostRedisplay();
-}
-
-void keyboard(unsigned char k, int x, int y) {
-    switch (k) {
-        case 27: // 27 = esc-button
-            cleanup();
-            exit(0);
-            break;
-        default:
-            break;
-    }
-}
-
-int setupGLUT(int *argc, char *argv[]) {
-    // setup the GLUT library which handles the windows for us
-    glutInit(argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-    glutInitWindowPosition(100,100);
-
-    int winid = glutCreateWindow("Virtual Reality Lab: OpenSG");
-
-    glutReshapeFunc(reshape);
-    glutDisplayFunc(display);
-    glutMouseFunc(mouse);
-    glutMotionFunc(motion);
-    glutKeyboardFunc(keyboard);
-//	glutPassiveMotionFunc(motion);
-    glutIdleFunc(display);
-
-    return winid;
-}
-
-
-void cleanup() {
-    beachTrans = NULL;
-    mgr = NULL;
 }
