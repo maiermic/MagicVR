e2ecf16ce4952e530ab838c6fd70730ba618dbd6
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-09-23 15:47:25 +0200

    create GlutFramework for desktop application
    It uses the SimpleSceneManager.

---
+++ includes/magicvr/MagicVrDesktopGlutFramework.hpp
@@ -0,0 +1,30 @@
+#ifndef MAGICVR_MAGICVRDESKTOPGLUTFRAMEWORK_HPP
+#define MAGICVR_MAGICVRDESKTOPGLUTFRAMEWORK_HPP
+
+#include "glutFramework/GlutFramework.hpp"
+#include <OpenSG/OSGSimpleSceneManager.h>
+#include <Scene.hpp>
+
+class MagicVrDesktopGlutFramework : public glutFramework::GlutFramework {
+    OSG::SimpleSceneManagerRefPtr mgr; // gets created in createWindow
+    Scene &scene;
+public:
+    MagicVrDesktopGlutFramework(Scene &scene);
+
+    int createWindow() override;
+
+    void keyboardDown(unsigned char key, int x, int y) override;
+
+    OSG::Node *root();
+
+    void mouseButtonPress(int button, int state, int x, int y) override;
+
+    void mouseMove(int x, int y) override;
+
+    void display(float dTime) override;
+
+    void reshape(int width, int height) override;
+};
+
+
+#endif //MAGICVR_MAGICVRDESKTOPGLUTFRAMEWORK_HPP
