891a246792c7c987f2127f4817e7f8bae9cdc699
Michael Maier <maiermic@cip.ifi.lmu.de>
2017-10-05 23:28:02 +0200

    remove unused includes

--- includes/magicvr/animation/BubbleAnimationsNode.hpp
+++ includes/magicvr/animation/BubbleAnimationsNode.hpp
@@ -1,40 +1,5 @@
 #pragma once
 
-#include <OpenSG/OSGGL.h>
-#include <OpenSG/OSGComponentTransform.h>
-#include <OpenSG/OSGComponentTransformBase.h>
-#include <OpenSG/OSGComponentTransformFields.h>
-#include <magicvr/animation/ParallelAnimation.hpp>
-#include <magicvr/ComponentTransformNode.hpp>
-#include <magicvr/node/TrajectoryContainerNode.hpp>
-#include <input/Tracker.hpp>
-#include <magicvr/node/MovableNode.hpp>
-#include <magicvr/animation/BubbleAnimationsNode.hpp>
-
-#include <OpenSG/OSGSceneFileHandler.h>
-#include <OpenSG/OSGQuaternion.h>
-#include <OpenSG/OSGMaterialGroup.h>
-#include <OpenSG/OSGImage.h>
-#include <OpenSG/OSGSimpleTexturedMaterial.h>
-#include <magicvr/ComponentTransformNode.hpp>
-#include <magicvr/animation/TranslationAnimation.hpp>
-#include <magicvr/animation/BezierCurve.hpp>
-#include <magicvr/node/TrajectoryContainerNode.hpp>
-#include <magicvr/animation/BezierAnimation.hpp>
-#include <magicvr/animation/EaseInOutAnimation.hpp>
-#include <magicvr/Spiral.hpp>
-#include <range/v3/to_container.hpp>
-#include <magicvr/ranges/view/Circle.hpp>
-#include <magicvr/ranges/view/rotate.hpp>
-#include <magicvr/animation/ScaleAnimation.hpp>
-#include <magicvr/animation/SequentialAnimation.hpp>
-#include "PathSettings.hpp"
-#include <magicvr/animation/BezierTranslationAnimation.hpp>
-#include <input/Tracker.hpp>
-#include <OpenSG/OSGSimpleGeometry.h>
-#include <magicvr/animation/AnimationContainer.hpp>
-
-
 #include <vector>
 #include <magicvr/ComponentTransformNode.hpp>
 #include <magicvr/animation/Animation.hpp>
